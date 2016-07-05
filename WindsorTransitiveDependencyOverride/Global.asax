8e5babff9632dae663bc92d8c71a708f35ef8660
Michael Kropat <michael.kropat@rackspace.com>
2016-07-05 16:52:16 -0400

    New project

---
+++ WindsorTransitiveDependencyOverride/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WindsorTransitiveDependencyOverride.MvcApplication" Language="C#" %>
---
+++ WindsorTransitiveDependencyOverride/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WindsorTransitiveDependencyOverride.Startup))]
+namespace WindsorTransitiveDependencyOverride
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
